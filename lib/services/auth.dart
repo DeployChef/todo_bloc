eb9bacb0228aab976a0ea44ebf7fe01bcec6f340
Vladislav <dc.titovvlad@gmail.com>
2023-06-21 23:02:29 +0300

    add auth service

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,3 +1,17 @@
-class Auth{
-  
+import 'package:hive/hive.dart';
+import 'package:todo_bloc/model/user.dart';
+
+class Auth {
+  late Box<User> _users;
+
+  Future init() async {
+    Hive.registerAdapter(UserAdapter());
+    _users = await Hive.openBox<User>("usersBox");
+  }
+
+  Future<String?> authUser(final String username, final String password) async {
+    final success = _users.values.any((element) =>
+        element.username == username && element.password == password);
+    return success ? username : null;
+  }
 }
